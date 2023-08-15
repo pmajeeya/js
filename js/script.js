48e1893e57a930aad2df5b4fafdc54ba1017a123
pmajeeya <133582855+pmajeeya@users.noreply.github.com>
2023-08-15 12:19:48 +0700

    ex.1

--- js/script.js
+++ js/script.js
@@ -1,1 +1,19 @@
-console.log(1);
+const numberOfFilms = +prompt('сколько фильмов вы уже смотрели?', ''),
+	personalMovieDB = {
+		count: numberOfFilms,
+		movies: {},
+		actors: {},
+		genres: [],
+		privat: false
+	};
+const one = prompt('one of last watched movies?', ''),
+	two = prompt('your rating?', ''),
+	three = prompt('one of last watched movies?', ''),
+	four = prompt('your rating?', '');
+
+personalMovieDB.movies[one] = two;
+personalMovieDB.movies[three] = four;
+
+
+console.log(personalMovieDB);
+
