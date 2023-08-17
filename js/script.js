fcf3fbfcaceaa860720d052b678104cb0359717c
pmajeeya <133582855+pmajeeya@users.noreply.github.com>
2023-08-17 12:49:38 +0700

    day 3 exc tree pattern

--- js/script.js
+++ js/script.js
@@ -1,19 +1,21 @@
-const numberOfFilms = +prompt('сколько фильмов вы уже смотрели?', ''),
-	personalMovieDB = {
-		count: numberOfFilms,
-		movies: {},
-		actors: {},
-		genres: [],
-		privat: false
-	};
-const one = prompt('one of last watched movies?', ''),
-	two = prompt('your rating?', ''),
-	three = prompt('one of last watched movies?', ''),
-	four = prompt('your rating?', '');
-
-personalMovieDB.movies[one] = two;
-personalMovieDB.movies[three] = four;
+//      *
+//     ***
+//    *****
+//   *******
+//  *********
+// ***********
 
 
-console.log(personalMovieDB);
-
+const k = 6;
+let result = '';
+// Проверяется именно переменная result, формируйте строку в ней
+for(let i = 0; i < k; i++){
+	for (let j = 0; j < k - i; j++){
+		result += ' ';
+	}
+	for(let j = 0; j < 2 * i + 1; j++){
+		result += '*';
+	}
+	result += '\n';
+} 
+console.log(result);
