1ea569960a781f4dd13e160e6ef6e9487add60b0
pmajeeya <133582855+pmajeeya@users.noreply.github.com>
2023-08-20 10:39:54 +0700

    day x
    :(

--- js/script.js
+++ js/script.js
@@ -1,48 +1,40 @@
-/* Задание на урок:
+// Место для первой задачи
 
-1) Автоматизировать вопросы пользователю про фильмы при помощи цикла
-
-2) Сделать так, чтобы пользователь не мог оставить ответ в виде пустой строки,
-отменить ответ или ввести название фильма длинее, чем 50 символов. Если это происходит - 
-возвращаем пользователя к вопросам опять. (К любой строке можно обратиться как 
-str.length - и получить её длину)
-
-3) При помощи условий проверить  personalMovieDB.count, и если он меньше 10 - вывести сообщение
-"Просмотрено довольно мало фильмов", если от 10 до 30 - "Вы классический зритель", а если больше - 
-"Вы киноман". А если не подошло ни к одному варианту - "Произошла ошибка"
-
-4) Потренироваться и переписать цикл еще двумя способами*/
-
-'use strict';
-
-const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
-if(personalMovieDB.count < 10){
-	alert('Просмотрено довольно мало фильмов.');
-}	else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30){
-	alert('Вы классический зритель.');
-} else if(personalMovieDB.count >= 30){
-	alert('Вы киноман.');
-} else {
-	console.log('Произошла ошибка.');
+function sayHello(name) {
+	return `Привет, ${name}!`;
 }
-const personalMovieDB = {
-	count: numberOfFilms,
-	movies: {},
-	actors: {},
-	genres: [],
-	private: false
-};
-for (let i = 0; i < 2; i++) {
-	
-	const a = prompt('Один из последних просмотренных фильмов?', ''),
-		  b = prompt('На сколько оцените его?', '');
-	
-	personalMovieDB.movies[a] = b;
-	if (a != null && b !=null && a != '' && b != '' && a.length < 50){
-		console.log('done!');
-	} else {
-		console.log('error!');
-		i--;
-	}	
+console.log(sayHello('Антон'));
+// Место для второй задачи
+function returnNeighboringNumbers(num) {
+	return [num - 1, num, num + 1];
 }
-console.log(personalMovieDB);
+console.log(returnNeighboringNumbers(4));
+// Место для третьей задачи
+function getMathResult(one, two) {
+	let result = one;
+	for(let i = 0, m = 2; i < two - 1; i++){
+		if(two <= 0 || typeof(two) != 'number'){
+			return one;
+		}
+		result +='---' + one * m;
+		m++;
+	}
+	return result;
+}
+
+console.log(getMathResult(10, 3));
+
+
+console.log(typeof('2'));
+
+// Вызов функции getMathResult(5, 3) даст ответ 5---10---15
+
+// Вызов функции getMathResult(3, 10) даст ответ 3---6---9---12---15---18---21---24---27---30
+
+// Вызов функции getMathResult(10, 5) даст ответ 10---20---30---40---50
+
+// Вызов функции getMathResult(10, '5') даст ответ 10
+
+// Вызов функции getMathResult(10, 0) даст ответ 10
+
+// Вызов функции getMathResult(20, -5) даст ответ 20
