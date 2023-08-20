91fb47bd25385e5228ce46fe9dee133c06bb1643
pmajeeya <133582855+pmajeeya@users.noreply.github.com>
2023-08-20 15:13:06 +0700

    another ex-e..

--- js/script.js
+++ js/script.js
@@ -89,3 +89,54 @@ function showMyDB(hidden){
 	}
 }
 showMyDB(personalMovieDB.private);
+
+
+
+
+
+/////
+
+// V=a^3
+// A=6a^2
+
+
+
+function calculateVolumeAndArea(edge){
+	cubeArea = 6 * (edge * edge);
+	cubeVolume = edge * edge * edge;
+	if (edge <= 0 || isNaN(edge) === true || edge %1 !== 0){
+		return 'При вычислении произошла ошибка';
+	}
+	return `volume of cube ${cubeVolume}, area - ${cubeArea}`;
+	`Объем куба: ${cubeVolume}, площадь всей поверхности: ${cubeArea}`;
+}
+
+console.log(calculateVolumeAndArea(12.8986433395));
+
+
+function getCoupeNumber(ticket) {
+	if(ticket === 0 || ticket > 36){
+		return 'Таких мест в вагоне не существует';
+	} else if(isNaN(ticket) || ticket %1 !== 0 || ticket < 0){
+		return 'Ошибка. Проверьте правильность введенного номера места';
+	} else {
+		return Math.ceil(ticket / 4);
+	}
+}
+console.log(getCoupeNumber('hello'));
+
+/*
+  => 9
+
+getCoupeNumber(7)  => 2
+
+getCoupeNumber(300)  => "Таких мест в вагоне не существует"
+
+getCoupeNumber(0)  => "Таких мест в вагоне не существует"
+
+getCoupeNumber(7.7)  => "Ошибка. Проверьте правильность введенного номера места"
+
+getCoupeNumber(-10)  => "Ошибка. Проверьте правильность введенного номера места"
+
+getCoupeNumber('Hello')  => "Ошибка. Проверьте правильность введенного номера места"
+*/
