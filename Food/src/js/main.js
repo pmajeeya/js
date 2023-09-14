e51f409a605d102a36d6e1ca844fedc81510ebe5
pmajeeya <133582855+pmajeeya@users.noreply.github.com>
2023-09-14 10:10:43 +0700

    timer

--- Food/src/js/main.js
+++ Food/src/js/main.js
@@ -2,18 +2,56 @@ document.addEventListener('DOMContentLoaded', () => {
 
 const tabs = document.querySelectorAll('.tabheader__item'),
         tabsContent = document.querySelectorAll('.tabcontent'),
-        tabsParent = document.querySelector('.tabheader__items');
+        tabsParent = document.querySelector('.tabheader__items'),
+        modal = document.querySelector('.modal'),
+        openModalBtn = document.querySelectorAll('[data-modal]'),
+        closeModalBtn = document.querySelector('[data-close]');
         
-        //timer
+
+        //modal
+openModalBtn.forEach(btn => {
+    btn.addEventListener('click', openModal);
+});
+
+function openModal(){
+    modal.style.display = 'block';
+    document.body.style.overflow = 'hiddem';
+}
+function closeModal(){
+    modal.style.display = 'none';
+    document.body.style.overflow = '';
+};
+closeModalBtn.addEventListener('click', closeModal);
+
+modal.addEventListener('click', (e) => {
+    if(e.target === modal){
+        modal.style.display = 'none';
+        document.body.style.overflow = '';
+    }
+});
+document.addEventListener('keydown', (e) => {
+    if(e.code === 'Escape' && modal.style.display === 'block'){
+        closeModal();
+    }
+});
+    //timer
         
 let deadline = '2023-09-20';
         
 function calcTime(endtime) {
-    let t = Date.parse(endtime) - Date.parse(new Date()),
-    days = Math.floor(t / 1000 / 60 / 60 / 24),
-    hours = Math.floor(t / 1000 / 60 / 60) % 24,
-    minutes = Math.floor(t / 1000 / 60) % 60,
-    seconds = Math.floor(t / 1000) % 60;
+    let days, hours, minutes, seconds;
+    let t = Date.parse(endtime) - Date.parse(new Date());
+    if(t <= 0){
+        days = 0;
+        hours = 0;
+        minutes = 0;
+        seconds = 0;
+    } else {
+        days = Math.floor(t / 1000 / 60 / 60 / 24),
+        hours = Math.floor(t / 1000 / 60 / 60) % 24,
+        minutes = Math.floor(t / 1000 / 60) % 60,
+        seconds = Math.floor(t / 1000) % 60;
+    } 
 return {
     't': t,
     'days':  days,
@@ -24,15 +62,20 @@ return {
 }
 
 inputValuesInTimer = function(selector, endtime) {
-    const timer = document.querySelector(selector);
-    const days = timer.querySelector('#days');
-    const hours = timer.querySelector('#hours');
-    const minutes = timer.querySelector('#minutes');
-    const seconds = timer.querySelector('#seconds');
+    const timer = document.querySelector(selector),
+          days = timer.querySelector('#days'),
+          hours = timer.querySelector('#hours'),
+          minutes = timer.querySelector('#minutes'),
+          seconds = timer.querySelector('#seconds'),
+          timeInterval = setInterval(updateClock, 1000);
     
+    updateClock();
+
     function getZero(num) {
         if(num < 10 && num >= 0){
         return `0${num}`;
+        } else if (num < 0){
+            return '00';
         } else return num;
     }
 
@@ -44,12 +87,17 @@ inputValuesInTimer = function(selector, endtime) {
         hours.innerHTML = getZero(t.hours);
         minutes.innerHTML = getZero(t.minutes);
         seconds.innerHTML = getZero(t.seconds);
-    }
 
+        if(t.t <= 0){
+            clearInterval(timeInterval);
+        }
+    }
     // calcTime(timer).forEach(v) = function(){
     //     v.innerHTML = `${indexOf(calcTime).value}`;
     // }
     }
+
+
 inputValuesInTimer('.timer', deadline)
 
         //tabs
