ea07d7f3d0d69f9a80dd6728f183d4f63de6ce17
pmajeeya <133582855+pmajeeya@users.noreply.github.com>
2023-09-15 16:38:42 +0700

    cards

--- Food/src/js/main.js
+++ Food/src/js/main.js
@@ -7,6 +7,70 @@ const tabs = document.querySelectorAll('.tabheader__item'),
         openModalBtn = document.querySelectorAll('[data-modal]'),
         closeModalBtn = document.querySelector('[data-close]');
         
+    // 
+
+
+class MenuCard {
+    constructor(src, alt, title, descr, price, parentSelector, ...classes) {
+        this.title = title;
+        this.descr = descr;
+        this.price = price;
+        this.src = src;
+        this.alt = alt;
+        this.parent = document.querySelector(parentSelector) || document.querySelector('.menu .container');
+        this.classes = classes;
+        this.rate = 9;
+        this.changeToUAH();
+    }
+    changeToUAH() {
+        this.price = this.price * this.rate;
+    };
+    render() {
+    const element = document.createElement('div')
+    if (this.classes.length === 0) {
+        this.classes = "menu__item";
+        element.classList.add(this.classes);
+    } else {
+        this.classes.forEach(className => element.classList.add(className));
+    }
+    element.innerHTML = `
+                <img src="${this.src}" alt="${this.alt}">
+                <h3 class="menu__item-subtitle">“${this.title}”</h3>
+                <div class="menu__item-descr">${this.descr}</div>
+                <div class="menu__item-divider"></div>
+                <div class="menu__item-price">
+                    <div class="menu__item-cost">Цена:</div>
+                    <div class="menu__item-total"><span>${this.price}</span> грн/день</div>
+                </div>
+                `
+    this.parent.append(element);
+    }
+}
+
+new MenuCard(
+    'img/tabs/vegy.jpg',
+    'vegy',
+    'Меню "Фитнес"',
+    'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
+    23,
+    ".menu .container").render();
+
+new MenuCard(
+    'img/tabs/elite.jpg',
+    'elite',
+    'Меню “Премиум”',
+    'В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!',
+    61,
+    '.menu .container').render();
+
+new MenuCard(
+    'img/tabs/post.jpg',
+    'post',
+    'Меню "Постное"',
+    'Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков..',
+    47).render();
+
+
 
         //modal
 openModalBtn.forEach(btn => {
@@ -15,7 +79,8 @@ openModalBtn.forEach(btn => {
 
 function openModal(){
     modal.style.display = 'block';
-    document.body.style.overflow = 'hiddem';
+    document.body.style.overflow = 'hidden';
+    clearInterval(modalTimerId);
 }
 function closeModal(){
     modal.style.display = 'none';
@@ -34,6 +99,16 @@ document.addEventListener('keydown', (e) => {
         closeModal();
     }
 });
+const modalTimerId = setInterval(openModal, 5000);
+
+function showModalByScroll(){
+    if (window.scrollY + document.documentElement.clientHeight >= document.documentElement.scrollHeight - 1){
+        openModal();
+        window.removeEventListener('scroll', showModalByScroll)
+    }
+}
+
+window.addEventListener('scroll', showModalByScroll);
     //timer
         
 let deadline = '2023-09-20';
